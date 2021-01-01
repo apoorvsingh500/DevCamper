fc562ba5421c14bb6498d64953335e81dfca0fe2
Apoorv Singh <sapoorv075@gmail.com>
2021-01-01 18:26:48 +0530

    Section 4 Done! (4/11)

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+const colors = require('colors')
 
-// Route files
-const bootcamps = require('./routes/bootcamps');
 // Load env vars
 dotenv.config({ path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 
 // Dev logging middleware
 if(process.env.NODE_ENV ==='development') {
@@ -16,9 +26,17 @@ if(process.env.NODE_ENV ==='development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-
-
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+        ));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
