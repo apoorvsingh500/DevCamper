eabd1a4eb1f5539d4e0b6d460e7a65a4fdff05cc
Apoorv Singh <sapoorv075@gmail.com>
2021-01-04 01:57:13 +0530

    Section 9 Done! (9/11)

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
 
 // Dev logging middleware
 if(process.env.NODE_ENV ==='development') {
@@ -41,6 +43,8 @@ app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
